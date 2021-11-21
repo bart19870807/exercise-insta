2656bb673577150e57c48670893ca31d4324ca3e
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-11-21 07:59:01 +0000

    accordion

--- script.js
+++ script.js
@@ -74,32 +74,40 @@
 //// RECORDER
 
 
-let btn = document.querySelector('.record-btn')
-btn.addEventListener('click', async function() {
-    let stream = await navigator.mediaDevices.getDisplayMedia({
-        video: true
+// let btn = document.querySelector('.record-btn')
+// btn.addEventListener('click', async function() {
+//     let stream = await navigator.mediaDevices.getDisplayMedia({
+//         video: true
+//     })
+//     const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
+//         ? "video/webm; codecs=vp9"
+//         : "video/webm"
+//     let mediaRecorder = new MediaRecorder(stream, {
+//         mimeType: mime
+//     })
+//     let chunks = []
+//     mediaRecorder.addEventListener('dataavailable', function(e){
+//         chunks.push(e.data)
+//     })
+//     mediaRecorder.addEventListener('stop', function(){
+//         let blob = new Blob(chunks, {
+//             type: chunks[0].type
+//         })
+//         let url = URL.createObjectURL(blob)
+//         let video = document.querySelector('video')
+//         video.src = url
+//         let a = document.createElement('a')
+//         a.href = url
+//         a.download = 'video.webm'
+//         a.click()
+//     })
+//     mediaRecorder.start()
+// })
+
+// ACCordion
+const accordion = document.getElementsByClassName('container');
+for (i=0; i<accordion.length; i++){
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active')
     })
-    const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
-        ? "video/webm; codecs=vp9"
-        : "video/webm"
-    let mediaRecorder = new MediaRecorder(stream, {
-        mimeType: mime
-    })
-    let chunks = []
-    mediaRecorder.addEventListener('dataavailable', function(e){
-        chunks.push(e.data)
-    })
-    mediaRecorder.addEventListener('stop', function(){
-        let blob = new Blob(chunks, {
-            type: chunks[0].type
-        })
-        let url = URL.createObjectURL(blob)
-        let video = document.querySelector('video')
-        video.src = url
-        let a = document.createElement('a')
-        a.href = url
-        a.download = 'video.webm'
-        a.click()
-    })
-    mediaRecorder.start()
-})
+}
