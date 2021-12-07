aa0e126172d4da1070aee43c60c3c4db725af69c
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-12-07 02:07:29 +0000

    text morph

--- script.js
+++ script.js
@@ -156,3 +156,135 @@
 //     styleSheet.setProperty('--color-1', color2);
 //     styleSheet.setProperty('--color-2', color1);
 // });
+
+// BMI Calculator
+
+// let age = document.getElementById('age');
+// let height = document.getElementById('height');
+// let weight = document.getElementById('weight');
+// let male = document.getElementById('m');
+// let female = document.getElementById('f');
+// let form = document.getElementById('form');
+
+// function validateForm() {
+//     if(age.value == "" || height.value =="" || weight.value =="" || (male.checked==false && female.checked== false)){alert('All fields are reguired');
+// document.getElementById('submit').removeEventListener('click', countBmi);}
+// else{
+//     countBmi();
+// }
+// }
+// document.getElementById('submit').addEventListener('click', validateForm);
+
+// function countBmi() {
+//     let p = [age.value, height.value, weight.value];
+//     if(male.checked){
+//         p.push('male');
+//     }else if(female.checked){
+//         p.push('female');
+//     }
+//     form.reset();
+//     let bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+//     let result = '';
+//     if(bmi<18.5){
+//         result = 'Underweight';
+//     }else if (18.5<=bmi && bmi<=24.9){
+//         result = 'Healthy';
+//     }
+//     else if (25<=bmi && bmi<=29.9){
+//         result = 'Overweight';
+//     }
+//     else if (30<=bmi && bmi<=34.9){
+//         result = 'Obese';
+//     }
+//     else if (35<=bmi){
+//         result = 'Extremely obese';
+//     }
+
+//     let h1 = document.createElement('h1');
+//     let h2 = document.createElement('h2');
+//     let t = document.createTextNode(result);
+//     let b = document.createTextNode('BMI: ');
+//     let r = document.createTextNode(parseFloat(bmi).toFixed(2));
+//     h1.appendChild(t);
+//     h2.appendChild(b);
+//     h2.appendChild(r);
+
+//     document.body.appendChild(h1);
+//     document.body.appendChild(h2);
+
+//     document.getElementById('submit').removeEventListener('click', countBmi);
+//     document.getElementById('submit').removeEventListener('click', validateForm);
+
+// }
+// document.getElementById('submit').addEventListener('click', countBmi);
+
+// TEXT MORPH
+
+const elts = {
+    text1: document.getElementById('text1'),
+    text2: document.getElementById('text2')
+}
+const texts = [
+    'CODEWITHRANDOM',
+    'is',
+    'so',
+    'satisfying',
+    'to',
+    'watch?'
+];
+const morphTime = 1;
+const cooldownTime = 0.25;
+let textIndex = texts.length -1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph () {
+    morph -= cooldown;
+    cooldown = 0;
+    let fraction = morph/morphTime;
+    if(fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+    }
+    setMorph(fraction);
+}
+function setMorph(fraction) {
+    //fraction = Math.cos(fraction * MathPI)/ -2 + .5;
+    elts.text2.style.filter = `blur(${Math.min(8/fraction -8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4)*100}%`;
+    fraction = 1 - fraction;
+
+    elts.text1.style.filter = `blur(${Math.min(8/fraction -8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4)*100}%`;
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length]
+}
+
+function doCooldown() {
+    morph = 0;
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "100%";
+}
+function animate() {
+    requestAnimationFrame(animate);
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+    cooldown -= dt;
+    if(cooldown <= 0) {
+        if(shouldIncrementIndex) {
+            textIndex ++;
+        }
+        doMorph();
+    } else{
+        doCooldown();
+    }
+}
+animate();
