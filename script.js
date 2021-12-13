22e18089382e70e3b68c08e787c1846248ae12e3
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-12-13 03:40:31 +0000

    signin sign up

--- script.js
+++ script.js
@@ -313,4 +313,22 @@
 // upButton.addEventListener('click', () =>changeSlide('up'));
 // downButton.addEventListener('click', () =>changeSlide('down'));
 
-// sign in login form
+sign in login form
+
+const signInBtn = document.getElementById('signIn');
+const signUpBtn = document.getElementById('signUp');
+const firstForm = document.getElementById('form1');
+const secondForm = document.getElementById('form2');
+const container = document.querySelector('.container');
+
+signInBtn.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+signUpBtn.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+firstForm.addEventListener('submit', (e) => e.preventDefault());
+secondForm.addEventListener('submit', (e) => e.preventDefault());
+
+// DICTIONARY APP
