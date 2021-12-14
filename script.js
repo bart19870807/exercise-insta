a7b98c8702778a626082b242c66b875b99aab56f
Bartlomiej Stachowski <bart.stachowski@gmail.com>
2021-12-14 03:40:40 +0000

    dictionary app

--- script.js
+++ script.js
@@ -313,22 +313,44 @@
 // upButton.addEventListener('click', () =>changeSlide('up'));
 // downButton.addEventListener('click', () =>changeSlide('down'));
 
-sign in login form
+// sign in login form
 
-const signInBtn = document.getElementById('signIn');
-const signUpBtn = document.getElementById('signUp');
-const firstForm = document.getElementById('form1');
-const secondForm = document.getElementById('form2');
-const container = document.querySelector('.container');
+// const signInBtn = document.getElementById('signIn');
+// const signUpBtn = document.getElementById('signUp');
+// const firstForm = document.getElementById('form1');
+// const secondForm = document.getElementById('form2');
+// const container = document.querySelector('.container');
 
-signInBtn.addEventListener('click', () => {
-    container.classList.remove('right-panel-active');
-});
+// signInBtn.addEventListener('click', () => {
+//     container.classList.remove('right-panel-active');
+// });
 
-signUpBtn.addEventListener('click', () => {
-    container.classList.add('right-panel-active');
-});
-firstForm.addEventListener('submit', (e) => e.preventDefault());
-secondForm.addEventListener('submit', (e) => e.preventDefault());
+// signUpBtn.addEventListener('click', () => {
+//     container.classList.add('right-panel-active');
+// });
+// firstForm.addEventListener('submit', (e) => e.preventDefault());
+// secondForm.addEventListener('submit', (e) => e.preventDefault());
 
 // DICTIONARY APP
+const wrapper = document.querySelector(".wrapper"),
+searchInput = wrapper.querySelector("input"),
+volume = wrapper.querySelector(".word i"),
+infoText = wrapper.querySelector(".info-text"),
+synonyms = wrapper.querySelector(".synonyms .list"),
+removeIcon = wrapper.querySelector(".search span");
+let audio;
+
+function data(result, word) {
+    if(result.title){
+        infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please try to search anoher word`
+    }else{
+        wrapper.classList.add('active');
+        let definition = result[0].meanings[0].definitions[0],
+        phontetics = `${result[0].meanings[0].partOfSpeech}/${result[0].phonetics[0].text/}`;
+        document.querySelector('word p').innerText = result[0].word;
+        document.querySelector('word span').innerText = phontetics;
+        document.querySelector('meaning span').innerText = definitions.definition;
+        document.querySelector('example span').innerText = definitions.example;
+        audio = new Audio('https' + result[0].phonetics[0].audio);
+    }
+}
